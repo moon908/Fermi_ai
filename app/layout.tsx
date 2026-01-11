b9cf19e86e895da8531ae45c8b53de9af7783717
Siddhesh Moon <siddheshmoon908u@gmail.com>
2026-01-11 13:03:36 +0530

    Implementing Sidebar

---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/ui/app-sidebar"
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "FermiAI",
+  description: "Generative AI Chatbot",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <SidebarProvider>
+          <AppSidebar />
+          <main className="flex-1 flex flex-col h-screen overflow-hidden relative">
+            <div className="absolute left-4 top-4 z-50">
+              <SidebarTrigger />
+            </div>
+            {children}
+          </main>
+        </SidebarProvider>
+      </body>
+    </html>
+  );
+}
